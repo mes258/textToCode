b250d91cf14588f8f7a6a38596eb0f1f13ce5f0b
mes258 <michael.smith.singapore@gmail.com>
2019-05-02 21:36:57 -0400

    Added zero to table

--- textToCode/textToCode/SpeechProcessor.swift
+++ textToCode/textToCode/SpeechProcessor.swift
@@ -23,6 +23,7 @@ class SpeechProcessor {
                                "or": "||",
                                "less": "<",
                                "greater": ">",
+                               "zero": "0",
                                "one": "1",
                                "two": "2",
                                "three": "3",
